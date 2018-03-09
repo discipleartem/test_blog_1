9ecf9d60b883fcc800bbe03c48a22bd7b0529cf3
Tomas <discipleartem@gmail.com>
2018-03-09 13:38:26 +0200

    fix server script and chage Procfile

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app
-web: python index.html runserver 0.0.0.0:$PORT
+web: python cgi-bin/wall.py runserver 0.0.0.0:$PORT
--- python3_web_server.py
+++ python3_web_server.py
@@ -1,4 +1,11 @@
+import os, sys
+
 from http.server import HTTPServer, CGIHTTPRequestHandler
-server_address = ("", 8000)
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
+# Путь до корня, где лежит cgi-bin
+webdir = r'D:\python_win\work\Blogs\test_blog_1'
+port = 8000
+# Переходим в корневую директорию
+os.chdir(webdir)
+srvaddr = ('127.0.0.1', port)
+srvrobj = HTTPServer(srvaddr, CGIHTTPRequestHandler)
+srvrobj.serve_forever()
